f0b80413602bf9899346a2c4a2a3d5805e0337f2
SuLG-k <nenashkinvov@gmail.com>
2024-01-31 04:05:28 +0700

    remove redundant cinema-list

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -23,6 +23,7 @@ include(":features:film-list:domain")
 include(":features:film-list:data")
 include(":features:film-list:presentation")
 include(":features:film-list:component")
+include(":features:root:component")
 
 include(":core:data-graphql")
 include(":core:di")
