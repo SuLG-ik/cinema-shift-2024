03ac7b0625561cbedb236b66eebc485d77e78f3f
SuLG-k <nenashkinvov@gmail.com>
2024-01-31 00:30:56 +0700

    add basic film item

--- features/film-list/ui/src/main/java/ru/sulgik/filmlist/ui/FilmListPreview.kt
+++ features/film-list/ui/src/main/java/ru/sulgik/filmlist/ui/FilmListPreview.kt
@@ -1,19 +1,23 @@
 package ru.sulgik.filmlist.ui
 
+import androidx.compose.foundation.layout.width
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
-@Preview
+@Preview(locale = "ru")
 @Composable
 fun FilmItemPreview() {
     FilmItem(
         film = Film(
-            title = "dicta",
-            subtitle = "inciderint",
+            title = "Title",
+            subtitle = "subtitle",
             userRating = Film.UserRating(imdb = 4.5f, kinopoisk = 6.7f),
             genres = listOf("Драма"),
             countryName = null,
             imageUrl = "https://www.google.com/#q=vim",
-        )
+        ),
+        modifier = Modifier.width(240.dp)
     )
 }
--- features/film-list/ui/src/main/java/ru/sulgik/filmlist/ui/FilmListScreen.kt
+++ features/film-list/ui/src/main/java/ru/sulgik/filmlist/ui/FilmListScreen.kt
@@ -6,10 +6,13 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.ui.res.stringResource
+import ru.sulgik.uikit.UIKitContainedButton
+import ru.sulgik.uikit.UIKitPaddingDefaultTokens
 
 data class Film(
     val title: String,
@@ -51,10 +54,38 @@ fun FilmItem(
         modifier = modifier,
     ) {
         Column(
-            modifier = Modifier.padding()
+            modifier = Modifier.padding(UIKitPaddingDefaultTokens.DefaultContentPadding)
         ) {
             FilmItemTitle(film)
+            FilmRating(film.userRating)
+            FilmAboutButton(onClick = {}, modifier = Modifier.fillMaxWidth())
         }
+    }
+}
+
+@Composable
+fun FilmAboutButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    UIKitContainedButton(
+        onClick = onClick,
+        largeCorners = false,
+        modifier = modifier,
+    ) {
+        Text(stringResource(id = R.string.more_info))
+    }
+}
+
+@Composable
+fun FilmRating(
+    rating: Film.UserRating,
+    modifier: Modifier = Modifier,
+) {
+    Column(
+        modifier = modifier,
+    ) {
+        Text("Kinoposik – ${rating.kinopoisk}", style = MaterialTheme.typography.bodySmall)
     }
 }
 
@@ -66,7 +97,7 @@ fun FilmItemTitle(
     Column(
         modifier = modifier,
     ) {
-        Text(film.title)
-        Text(film.subtitle)
+        Text(film.title, style = MaterialTheme.typography.bodyLarge)
+        Text(film.subtitle, style = MaterialTheme.typography.bodySmall)
     }
 }
