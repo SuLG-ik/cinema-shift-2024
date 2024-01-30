768490e6542176123b83e291ad46738d2000c3ae
SuLG-k <nenashkinvov@gmail.com>
2024-01-31 04:03:17 +0700

    fix settings.gradle.kts.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -12,10 +12,19 @@ dependencyResolutionManagement {
         mavenCentral()
     }
 }
+enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
 
 rootProject.name = "CinemaApp"
 include(":app")
 include(":uikit")
 
-include(":features:cinema-list:ui")
-include(":uikit")
+include(":features:film-list:ui")
+include(":features:film-list:domain")
+include(":features:film-list:data")
+include(":features:film-list:presentation")
+include(":features:film-list:component")
+
+include(":core:data-graphql")
+include(":core:di")
+include(":core:mvi")
+include(":core:component")
