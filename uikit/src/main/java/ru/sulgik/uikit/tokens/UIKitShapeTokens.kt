f1f2d517cdb54127f1aa4a798771be69aa0ced68
SuLG-k <nenashkinvov@gmail.com>
2024-02-01 00:49:26 +0700

    add uikit tabs

--- uikit/src/main/java/ru/sulgik/uikit/tokens/UIKitShapeTokens.kt
+++ uikit/src/main/java/ru/sulgik/uikit/tokens/UIKitShapeTokens.kt
@@ -7,4 +7,5 @@ internal object UIKitShapeTokens {
     val CornerLarge = RoundedCornerShape(32.dp)
     val CornerMedium = RoundedCornerShape(16.dp)
     val CornerSmall = RoundedCornerShape(8.0.dp)
+    val CornerNone = RoundedCornerShape(0.dp)
 }
