e62a6eb74c6def931bc18200790f038bcb73fde3
SuLG-k <nenashkinvov@gmail.com>
2024-01-31 17:00:09 +0700

    UIKitPaddingDefaultTokens.DefaultContentPadding up to 15.dp

--- uikit/src/main/java/ru/sulgik/uikit/tokens/UIKitPaddingDefaultTokens.kt
+++ uikit/src/main/java/ru/sulgik/uikit/tokens/UIKitPaddingDefaultTokens.kt
@@ -3,6 +3,6 @@ package ru.sulgik.uikit.tokens
 import androidx.compose.ui.unit.dp
 
 object UIKitPaddingDefaultTokens {
-    val DefaultContentPadding = 10.dp
+    val DefaultContentPadding = 15.dp
     val DefaultItemsBetweenSpace = 10.dp
 }
