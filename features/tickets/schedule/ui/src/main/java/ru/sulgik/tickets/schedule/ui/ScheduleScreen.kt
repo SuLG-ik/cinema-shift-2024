6aaf046c75bde16f6013aaf05f4cb614fb75a4e0
SuLG-k <nenashkinvov@gmail.com>
2024-02-01 00:50:10 +0700

    add uikit tabs support

--- features/tickets/schedule/ui/src/main/java/ru/sulgik/tickets/schedule/ui/ScheduleScreen.kt
+++ features/tickets/schedule/ui/src/main/java/ru/sulgik/tickets/schedule/ui/ScheduleScreen.kt
@@ -6,11 +6,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Tab
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import ru.sulgik.uikit.UIKitContainedButton
+import ru.sulgik.uikit.UIKitTab
+import ru.sulgik.uikit.UIKitTabRow
 import ru.sulgik.uikit.UIKitTopBar
 import ru.sulgik.uikit.tokens.UIKitPaddingDefaultTokens
 import java.time.LocalDate
@@ -83,8 +92,39 @@ fun ScheduleInfo(
             .padding(horizontal = UIKitPaddingDefaultTokens.DefaultContentPadding)
             .verticalScroll(rememberScrollState())
     ) {
+        ScheduleDates(
+            seances, modifier = Modifier.fillMaxWidth()
+        )
         seances.forEach {
             Text(text = it.date.toString() + it.time.toString())
+        }
+    }
+}
+
+@Composable
+fun ScheduleDates(
+    seances: List<Seance>,
+    modifier: Modifier = Modifier,
+) {
+    val selectedItem = remember { mutableIntStateOf(1) }
+    UIKitTabRow(
+        modifier = modifier,
+        selectedTabIndex = selectedItem.intValue,
+    ) {
+        seances.mapIndexed { index, it ->
+            UIKitTab(
+                selected = selectedItem.intValue == index,
+                onClick = { selectedItem.intValue = index },
+            ) {
+                Text(
+                    "${it.date.dayOfMonth}.${it.date.monthValue}",
+                    style = MaterialTheme.typography.headlineSmall,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .align(Alignment.CenterHorizontally),
+                )
+            }
         }
     }
 }
